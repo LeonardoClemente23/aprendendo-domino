1f828455fe096fbeb5c5a2ef6799099bcba0cf13
f6206672 Leonardo Clemente de Figueiredo Silva <leonardo.clemente@bb.com.br>
2022-09-29 21:05:58 -0300

    implementando estrutura de dados e RandomSelector

--- src/IA/app/RandomHashSet.java
+++ src/IA/app/RandomHashSet.java
@@ -2,13 +2,64 @@ package IA.app;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 
 public class RandomHashSet<T> {
     HashSet<T> set = new HashSet<>();
     ArrayList<T> data = new ArrayList<>();
 
+    public ArrayList<T> getData() {
+        return data;
+    }
+
     public boolean contains(T object) {
         return set.contains(object);
     }
 
+    public T randomElement() throws Exception {
+        if (set.size() > 0) {
+            Random random = new Random();
+            int bound = data.size();
+            return data.get(random.nextInt(bound));
+        }
+        throw new Exception("Empty Set.");
+    }
+
+    public void add(T object) {
+        if (!set.contains(object)) {
+            set.add(object);
+            data.add(object);
+        }
+    }
+
+    public void clear() {
+        set.clear();
+        data.clear();
+    }
+
+    public T getElement(int index) throws Exception {
+        if (index < 0 || index >= size()) {
+            throw new Exception("Invalid index.");
+        }
+        return data.get(index);
+    }
+
+    // public T getElementByTemplate(T template){}
+
+    public void remove(int index) throws Exception {
+        if (index < 0 || index >= size()) {
+            throw new Exception("Invalid index.");
+        }
+        set.remove(data.get(index));
+        data.remove(index);
+    }
+
+    public void remove(T object) {
+        set.remove(object);
+        data.remove(object);
+    }
+
+    private int size() {
+        return data.size();
+    }
 }
